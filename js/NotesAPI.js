0cb8a013f615472de911f19ea73bdf007cd73af7
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2022-01-16 17:37:55 +0530

    Complete data api

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -1,14 +1,34 @@
 export default class NotesAPI {
     static getAllNotes() {
         const notes = JSON.parse(localStorage.getItem('notesapp-notes') || '[]');
-        return notes;
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1
+        });
     }
 
     static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
 
+        const existingNotes = notes.find(note => note.id === noteToSave.id);
+        
+        if(existingNotes) {
+            // Edit/Update
+            existingNotes.title = noteToSave.title;
+            existingNotes.body = noteToSave.body;
+            existingNotes.updated = new Date().toISOString();
+        } else {
+            // Add/Insert New
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+        localStorage.setItem('notesapp-notes', JSON.stringify(notes));
     }
 
     static deleteNote(id) {
-
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
+        localStorage.setItem('notesapp-notes', JSON.stringify(newNotes));
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-import NotesAPI from './NotesAPI.js';
+import NotesAPI from "./NotesAPI.js";
+
+NotesAPI.deleteNote(487318)
 
 console.log(NotesAPI.getAllNotes());
