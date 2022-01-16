fdc7ba02d4ab4e2bdcfb95b3888c59cdc6b494f4
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2022-01-16 17:55:58 +0530

    Add events

---
+++ js/NotesView.js
@@ -0,0 +1,40 @@
+export default class NotesView {
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete} = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+            <div class="notes__sidebar">
+                <button class="notes__add" type="button">Add Note</button>
+                <div class="notes__list">
+                    
+                </div>
+            </div>
+            <div class="notes__preview">
+                <input type="text" placeholder="New Note.." class="notes__title">
+                <textarea class="notes__body">
+                    Take Note..
+                </textarea>
+            </div>
+        `;
+
+        const btnAddNote = this.root.querySelector('.notes__add');
+        const inpTitle = this.root.querySelector('.notes__title');
+        const inpBody = this.root.querySelector('.notes__body');
+
+        btnAddNote.addEventListener('click', () => {
+            this.onNoteAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener('blur', () => {
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
+
+                this.onNoteEdit(updatedTitle, updatedBody);
+            })
+        });
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 import NotesAPI from "./NotesAPI.js";
+import NotesView from "./NotesView.js";
 
-NotesAPI.deleteNote(487318)
+const app = document.getElementById("app");
+const view = new NotesView(app, {
+    onNoteAdd() {
+        console.log("Let's add a new note");
+    },
 
-console.log(NotesAPI.getAllNotes());
+    onNoteEdit(newTitle, newBody) {
+        console.log(newTitle);
+        console.log(newBody);
+    }
+});
